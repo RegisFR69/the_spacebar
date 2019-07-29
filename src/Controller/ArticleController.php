e30df4d92cfdb26b72d2e5ed9d4ca448f3751758
chichille <regis.coquelet@gmail.com>
2019-07-29 12:23:46 +0200

    making so much good progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show one space article: "%s"!',
+            $slug
+        ));
+    }
+
+
+}
