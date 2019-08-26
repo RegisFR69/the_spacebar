53b981d83b90fd55c0ff9433d3d7aef05e1e93bd
chichille <regis.coquelet@gmail.com>
2019-08-26 16:23:53 +0200

    season 3 chap 6 Generating a Twig Extension

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('cached_markdown', [$this, 'processMarkdown'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function processMarkdown($value)
+    {
+        return strtoupper($value);
+    }
+
+    public function doSomething($value)
+    {
+        // ...
+    }
+}
