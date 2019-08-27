805607070088ad8c216f88333d3494ef2fe1def8
chichille <regis.coquelet@gmail.com>
2019-08-27 13:17:08 +0200

    season 3 chap 7

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,12 +2,23 @@
 
 namespace App\Twig;
 
+use App\Service\MarkdownHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
+    /**
+     * @var MarkdownHelper
+     */
+    private $markdownHelper;
+
+    public function __construct(MarkdownHelper $markdownHelper)
+    {
+        $this->markdownHelper = $markdownHelper;
+    }
+
     public function getFilters(): array
     {
         return [
@@ -17,7 +28,7 @@ class AppExtension extends AbstractExtension
 
     public function processMarkdown($value)
     {
-        return strtoupper($value);
+        return $this->markdownHelper->parse($value);
     }
 
     public function doSomething($value)
