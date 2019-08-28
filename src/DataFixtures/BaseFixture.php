91a24c7aae3d060e6d2216bed0eb35ac9cc3d58a
chichille <regis.coquelet@gmail.com>
2019-08-28 13:18:49 +0200

    season 3 chap 16 StofDoctrineExtensionsBundle

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -4,18 +4,23 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
 
 abstract class BaseFixture extends Fixture
 {
     /** @var ObjectManager */
     private $manager;
-
     abstract protected function loadData(ObjectManager $manager);
+    /** @var Generator */
+    protected $faker;
+
+
 
     public function load(ObjectManager $manager)
     {
         $this->manager = $manager;
-
+        $this->faker = Factory::create();
         $this->loadData($manager);
     }
 
